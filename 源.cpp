78a475131ce5d065cd22d2ff9ad5c484be924491
gholost761 <2227045278@qq.com>
2024-11-11 22:38:57 +0800

    Add files via upload

---
+++ 源.cpp
@@ -0,0 +1,29 @@
+#include "stdio.h"
+int main()
+{
+
+	int x, y, z;
+	for (x = 1; x <= 9; x++)
+	{
+		printf("\n");
+		for (y = 1; y <= x; y++)
+		{
+			z = x * y;
+			printf("%d*%d=%2d ", x, y, z);
+		}
+	}
+	printf("\n");
+	printf("\n");
+	int a, b, c;
+	for (a = 9; a <= 9 && a > 0; a--)
+	{
+		printf("\n");
+		for (b = 1; b <= a; b++)
+		{
+			c = a * b;
+			printf("%d*%d=%2d ", a, b, c);
+		}
+	}
+	return 0;
+}
+
